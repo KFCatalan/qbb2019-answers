eb270a5ba0bf3edae7f3ecafca133b1b6a1562a4
Kevin Catalan <kevinfcatalan@gmail.com>
2019-09-02 18:15:39 -0400

    Day 3 lunch exercises

--- day3-lunch/exercise-01.py
+++ day3-lunch/exercise-01.py
@@ -2,6 +2,11 @@
 
 import sys
 
+"""
+Binary Search
+
+"""
+
 
 f = open(sys.argv[1])
 gene_list = []
@@ -19,38 +24,64 @@ for i, line in enumerate(f):
     gene_start = int(column[3])
     gene_stop = int(column[4])
     gene_name = column[13]
-    if chrom == "3R":
-        continue
-    if types == "gene":
+    if types == "gene" and chrom == "3R":
         if 'gene_biotype "protein_coding";' in line:
-            gene_list.append((gene_start, gene_stop, gene_name))
-    print(gene_list)
+            gene_list.append(gene_stop)
+            gene_list.append(gene_start)
+gene_list = sorted(gene_list)  
+#print(len(gene_list))  #print(gene_list)
 
 
-lo = 0
-hi = len(gene_list)-1
-mid = 0
+
+count = len(gene_list)
+
 number_iterations = 0
 
-while (lo <= hi):
-    mid = int((hi+lo) / 2)
+while count != 1:
     
     number_iterations += 1
 
-    if (search_pos < gene_list[mid][0]):
-        gene_list[lo:mid]
-        
-    elif (search_pos > gene_list[mid][1]):
-        lo = gene_list[mid]
-        gene_list[mid:hi]
+    if (search_pos < int(gene_list[int(count/2)])):
+        gene_list = gene_list[:int(count/2)]
 
-    else:
-        #gene_list[mid] spans the search_pos!
-        break
+    elif (search_pos > int(gene_list[int(count/2)])):
+        gene_list = gene_list[int(count/2):]
 
-print(gene_list[mid])
+    count = int(count/2)
+    
+location = gene_list[count]
+    
+print(location)
 
+# print(number_iterations)
+
+
+
+for i, line in enumerate(open(sys.argv[1])):
+    column = line.rstrip("\n").split()
+    if line.startswith("#"):
+        continue
+    chrom = column[0]
+    types = column[2]
+    gene_start = int(column[3])
+    gene_stop = int(column[4])
+    gene_name = column[13]
+    if types == "gene" and chrom == "3R" and "protein_coding" in line:
+            if str(location) in line:
+                gene_id = str(column[13])
+                print(gene_id)
+
+
+print(abs(location - search_pos))
 print(number_iterations)
+print(gene_id)
+
+
+
+
+
+
+
 
 
 
