2d74a5ceaf210358d17504fca3cea22040c3fd04
Kevin Catalan <kevinfcatalan@gmail.com>
2019-09-02 18:13:56 -0400

    Day 3 homework

--- day3-homework/exercise-02.py
+++ day3-homework/exercise-02.py
@@ -25,11 +25,14 @@ for ident, tsequence in treader:
     target_2_target_seq[ident] = tsequence #ident is key and qseq is value
     for i in range(0, len(tsequence) -k +1):
         kmer = tsequence[i:i+k]
+        if kmer not in kmers:
+            kmers[kmer] = [(i, ident)]
         if kmer in kmers:
             kmers[kmer].append((i, ident))
-        else:
-            kmers[kmer] = [(i, ident)]
-            
+
+# print(kmers)
+# print(target_2_target_seq)
+extended_kmers = []
 for ident, qsequence in qreader:
     qsequence = qsequence.upper()
     for j in range(0, len(qsequence) -k +1):
@@ -40,23 +43,41 @@ for ident, qsequence in qreader:
                 ts = len(tseq)              #length of the tseq
                 qs = len(qsequence)         #length of qseq
                 extend_right = True         # boolean sent 
-          extended_kmer = kmer 
-            while True:
-                if extend_right:
-                    if qsequence[j + k + 1] == tseq[i + k + 1]
-                        i += 1
-                        j += 1
-                        extended_kmer += tseq[i + k + 1]
-                    else:
+                extended_kmer = kmer
+                counter = 0
+                while True:
+                    # print(counter)
+                    if ts <= i + k + 1 or qs <= j + k + 1:
                         extend_right = False
-                        
-                else:
-                    break
-                 if ts == (i + k) or tseq == (j + k):
-                     extend_right = False
-                     #needs a print statement using 3rd dictionary
+                    if extend_right:
+                        # print("inner if",j+k+1)
+                        # print("inner if",len(qsequence))
+                        # print(len(tsequence))
+                        # print(i+k+1)
+                        if qsequence[j + k + 1] == tseq[i + k + 1]:
+                            i += 1
+                            j += 1
+                            extended_kmer += tseq[i + k + 1]
+                        else:
+                            extend_right = False
+                    else:
+                        break
+                    # print(i+k+1)
+                    # print("outer",j+k+1)
+                    # print(ts)
+                    # print("outer",qs)
+                    # counter +=1
+                extended_kmers.append((len(extended_kmer),extended_kmer))
+                     
+#needs a print statement using 3rd dictionary
 
-# extended_kmers.sort
+extended_kmers = sorted(extended_kmers,reverse=True)
+print(extended_kmers)
+    
+    
+for length,kmer in extended_kmers:
+    print(length,kmer,sep='\t')
+
 
 
 # uniq the match to avoid repeating
